36472a2d4408f4e704201571ff960aa1cf42be60
Tamara <tamara.nahamov@gmail.com>
2023-12-11 08:20:20 +0200

    update k8s files

---
+++ MicroRabbit/MicroRabbit.Orders.Data/Migrations/20231210210705_M2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroRabbit.Orders.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class M2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExternalId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Books_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Books_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Books");
+        }
+    }
+}
